970b4a484122f81a457f0c54a6fae3fb25ed8bd7
PoushnayaA <poushnaya@bk.ru>
2025-01-11 00:46:13 +0300

    add header, hero, advantages blocks

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,49 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
+import Swiper from './vendor/swiper';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
-
   iosVhFix();
 
-  // Modules
-  // ---------------------------------
+  new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
+
+  const advantagesItems = document.querySelectorAll('.advantages__item');
+
+  function changeHeight() {
+    advantagesItems.forEach((item) => {
+      item.style.height = 'auto';
+  });
+    let maxHeight = 0;
+
+    advantagesItems.forEach((item) => {
+      const itemHeight = item.offsetHeight;
+      if (itemHeight > maxHeight) {
+          maxHeight = itemHeight;
+      }
+  });
+
+
+  advantagesItems.forEach((item) => {
+      item.style.height = `${maxHeight}px`;
+  });
+}
+
+window.addEventListener('load', changeHeight);
+
+window.addEventListener('resize', changeHeight);
+
+
+
   window.addEventListener('load', () => {
     initModals();
     const form = new Form();
@@ -24,27 +52,3 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
